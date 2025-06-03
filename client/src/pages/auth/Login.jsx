e3b973e2c3de00b9949cd86216fc6c50e292feb0
muskiofficially <shahmadarmuskan48@gmail.com>
2025-06-03 10:47:37 +0530

    Muskan
    App created with vite react + tailwind css as well as header part for authentication page added

---
+++ client/src/pages/auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+   <>
+        <h1 className='text-5xl underline text-green-800 mt-4 text-center'>Login page</h1>
+   </>
+  )
+}
+
+export default Login
---
+++ client/src/pages/navbars/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+        //Header for navgitation bar
+            <header className='bg-[#3F3F3F] text-white p-4 shadow'>
+                <div className='max-w-7xl mx-auto flex items-center justify-between'>
+                    <h1 className='text-xl font-semibold'>Hastechy</h1>
+                </div> 
+            </header>
+        </>
+    )
+}
+
+export default Navbar
